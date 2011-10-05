d91df26c37c55f30f15ba776e21e4e45ceb5df24
Marco A. Martinez <mam002@ucsd.edu>
2011-10-05 16:49:07 -0700

    Changes accepted

--- setup.py
+++ setup.py
@@ -3,44 +3,44 @@ import os
 
 version = '0.4.5'
 
-setup(name='hive.lab',
-      version=version,
-      description="Lab Inventory management",
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='hive.lab',
+    version=version,
+    description="Lab Inventory management",
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='BEAST Core Development Team',
-      author_email='beast@ucsd.edu',
-      url='https://github.com/beastcore/hive.lab',
-      license='GPL',
-      packages=find_packages('src', exclude=['ez_setup']),
-      package_dir={'':'src'},
-      namespace_packages=['hive'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+    keywords='',
+    author='BEAST Core Development Team',
+    author_email='beast@ucsd.edu',
+    url='https://github.com/beastcore/hive.lab',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'':'src'},
+    namespace_packages=['hive'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
         'setuptools',
         'collective.autopermission', # Schema permissions
+        'lovely.session', # for session support
         'plone.app.dexterity', # Dexterity FTI
         'plone.behavior',
         'plone.app.registry', # For registering the behaviors
         'reportlab', # PDF label generator
         'xlutils', # Excel Export
         'z3c.form >= 2.4.1', # For attributes to work correctly
+
         'avrc.aeh',
         'avrc.data.store', # EAV Back-end
         'beast.browser', # Tools for browser displays
-        'lovely.session', # for session support
-      ],
+        ],
     extras_require=dict(
         test=['plone.app.testing'],
         ),
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
--- src/hive/lab/testing.py
+++ src/hive/lab/testing.py
@@ -2,21 +2,28 @@ from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
+
 from avrc.aeh.testing import CLINICAL_FIXTURE
 
 from zope.configuration import xmlconfig
 
+
 class LabLayer(PloneSandboxLayer):
 
     defaultBases = (CLINICAL_FIXTURE,)
-    
+
     def setUpZope(self, app, configurationContext):
         # Load ZCML
-        import hive.lab
-        xmlconfig.file('configure.zcml', hive.lab, context=configurationContext)
+        import hive.lab as package
+        xmlconfig.file('configure.zcml', package, context=configurationContext)
 
     def setUpPloneSite(self, portal):
         applyProfile(portal, 'hive.lab:default')
 
+
 LAB_FIXTURE = LabLayer()
-LAB_INTEGRATION_TESTING = IntegrationTesting(bases=(LAB_FIXTURE,), name="hive:Integration")
+
+LAB_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(LAB_FIXTURE,),
+    name='HiveLab:Integration'
+    )
