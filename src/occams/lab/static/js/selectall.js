9014e5dfb06b65422d41ccb90eeb76391872baba
David Mote <damote@ucsd.edu>
2013-03-08 13:48:18 -0800

    updated to use bitcore.widgets. NOTE: bitcore.widgets and crud forms don't play nicely, so occams.lab specifically removes the bitcore.widgets tags on crud forms.

--- src/occams/lab/static/js/selectall.js
+++ src/occams/lab/static/js/selectall.js
@@ -31,8 +31,8 @@
         }
       );
       // Set up the receipt printer for the aliquot checkout view
-      $("input[id='aliquot-checkout-buttons-printreceipt']").wrap('<a id="receiptprinter" href="./receipt" target="_new" />');
-      $("#receiptprinter").printPage();
+      // $("input[id='aliquot-checkout-buttons-printreceipt']").wrap('<a id="receiptprinter" href="./receipt" target="_new" />');
+      // $("#receiptprinter").printPage();
 
       // Set up overlay for the lab filter
       // No overlays for IE6
@@ -43,11 +43,24 @@
           subtype: 'ajax',
           filter: common_content_filter,
           formselector: '#content-core>#form',
-          noform: 'reload'
+          noform: 'reload',
+          config: {onLoad: function () {
+              $("select.bitcore-select2").select2();
+              $(function () {
+                  $("input.bitcore-datepicker").datepicker({
+                    changeMonth: true,
+                    changeYear: true
+                  });
+                });
+            },
+            onBeforeClose: function () {
+              $("select.bitcore-select2").select2("close");
+            }
+          }
         });
       }
       // Set up overlay for specimen label printer
-      $("input[id='crud-edit-form-buttons-print']").wrap('<a id="specimenlabelprinter" href="./printspecimenlabelform" />');
+  //     $("input[id='crud-edit-form-buttons-print']").wrap('<a id="specimenlabelprinter" href="./printspecimenlabelform" />');
 
       if (!$.browser.msie ||
           parseInt($.browser.version, 10) >= 7) {
@@ -57,6 +70,19 @@
           filter: common_content_filter,
           formselector: '#content-core>#form',
           noform: 'reload',
+          config: {onLoad: function () {
+              $("select.bitcore-select2").select2();
+              $(function () {
+                  $("input.bitcore-datepicker").datepicker({
+                    changeMonth: true,
+                    changeYear: true
+                  });
+                });
+            },
+            onBeforeClose: function () {
+              $("select.bitcore-select2").select2("close");
+            }
+          },
           closeselector: '[name=form.buttons.close]'
         });
       }
@@ -70,10 +96,23 @@
           filter: common_content_filter,
           formselector: '#content-core>#form',
           noform: 'reload',
+          config: {onLoad: function () {
+              $("select.bitcore-select2").select2();
+              $(function () {
+                  $("input.bitcore-datepicker").datepicker({
+                    changeMonth: true,
+                    changeYear: true
+                  });
+                });
+            },
+            onBeforeClose: function () {
+              $("select.bitcore-select2").select2("close");
+            }
+          },
           closeselector: '[name=form.buttons.close]'
         });
       }
-      // Set up overlay for batch updating aliquot pending checkout
+  //     // Set up overlay for batch updating aliquot pending checkout
       $("input[id='aliquot-checkout-buttons-batchupdate']").wrap('<a class="batchcheckout" href="./batchcheckoutform" />');
       if (!$.browser.msie ||
           parseInt($.browser.version, 10) >= 7) {
@@ -83,10 +122,23 @@
           filter: common_content_filter,
           formselector: '#content-core>#form',
           noform: 'reload',
+          config: {onLoad: function () {
+              $("select.bitcore-select2").select2();
+              $(function () {
+                  $("input.bitcore-datepicker").datepicker({
+                    changeMonth: true,
+                    changeYear: true
+                  });
+                });
+            },
+            onBeforeClose: function () {
+              $("select.bitcore-select2").select2("close");
+            }
+          },
           closeselector: '[name=form.buttons.close]'
         });
       }
-      // Set up the add specimen overlay
+  //     // Set up the add specimen overlay
       $("input[id='crud-edit-form-buttons-addspecimen']").wrap('<a class="addspecimen" href="./addspecimen" />');
       // Set up cycle listing for adding of specimen
       var buildCycleList = function () {
@@ -134,6 +186,7 @@
         $("#form-widgets-specimen_patient_our").blur(
           function () {
             buildCycleList();
+            $("#s2id_form-widgets-specimen_cycle").select2("val", "");
           }
         );
       };
@@ -144,10 +197,24 @@
         // Set up overlays
         $("a#addspecimen, a.addspecimen").prepOverlay({
           subtype: 'ajax',
-          config: {onLoad: function () {initializeSpecimenAdd(); }},
+          config: {onLoad: function () {
+              initializeSpecimenAdd();
+              $("select.bitcore-select2").select2();
+              $(function () {
+                  $("input.bitcore-datepicker").datepicker({
+                    changeMonth: true,
+                    changeYear: true
+                  });
+                });
+            },
+            onBeforeClose: function () {
+              $("select.bitcore-select2").select2("close");
+            }
+          },
           filter: common_content_filter,
           formselector: '#content-core>form#form',
           noform: 'reload',
+
           closeselector: '[name=form.buttons.cancel]'
         });
       }
