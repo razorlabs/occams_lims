81474ddcde54ef1baf970424a2ea7ee1dd64c2a3
Marco Martinez <mam002@ucsd.edu>
2014-03-27 11:23:20 -0700

    Minimal tweaks to get it to work with clinical v2

--- setup.py
+++ setup.py
@@ -1,46 +1,109 @@
-from setuptools import setup, find_packages
 import os
+from subprocess import Popen, PIPE
+from setuptools import setup, find_packages
+import sys
 
-version = '1.0.0b16'
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+REQUIRES = [
+    'alembic',
+    'babel',
+    'colander',
+    'cssmin',
+    'deform',
+    'humanize',
+    'jsmin',
+    'lingua',
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_deform',
+    'pyramid_layout',
+    'pyramid_mailer',
+    'pyramid_tm',
+    'pyramid_redis_sessions',
+    'pyramid_redis',
+    'pyramid_rewrite',
+    'pyramid_webassets',
+    'pyramid_who',
+    'reportlab',
+    'SQLAlchemy',
+    'six',
+    'transaction',
+    'webassets',
+    'xlutils',
+    'zope.sqlalchemy',
+    'zope.dottedname',
+
+    'occams.clinical',
+    'occams.datastore'
+]
+
+EXTRAS = {
+    'postgresql': ['psycopg2'],
+    'test': [
+        'pyramid_debugtoolbar',
+        'nose',
+        'coverage',
+        'WebTest',
+        'beautifulsoup4'],
+}
+
+
+def get_version():
+    version_file = os.path.join(HERE, 'VERSION')
+
+    # read fallback file
+    try:
+        with open(version_file, 'r+') as fp:
+            version_txt = fp.read().strip()
+    except:
+        version_txt = None
+
+    # read git version (if available)
+    try:
+        version_git = (
+            Popen(['git', 'describe'], stdout=PIPE, stderr=PIPE, cwd=HERE)
+            .communicate()[0]
+            .strip()
+            .decode(sys.getdefaultencoding()))
+    except:
+        version_git = None
+
+    version = version_git or version_txt or '0.0.0'
+
+    # update fallback file if necessary
+    if version != version_txt:
+        with open(version_file, 'w') as fp:
+            fp.write(version)
+
+    return version
 
 setup(
     name='occams.lab',
-    version=version,
+    version=get_version(),
     description="Lab Inventory management",
     classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        ],
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
     keywords='',
     author='BEAST Core Development Team',
     author_email='bitcore@ucsd.edu',
-    url='https://github.com/beastcore/occams.lab',
+    url='https://bitbucket.org/ucsdbitcore/occams.lab',
     license='GPL',
     packages=find_packages('src', exclude=['ez_setup']),
-    package_dir={'':'src'},
+    package_dir={'': 'src'},
     namespace_packages=['occams'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'setuptools',
-        'collective.autopermission', # Schema permissions
-        'collective.beaker', # for session support
-        'plone.app.dexterity [grok]', # Dexterity FTI
-        'plone.behavior',
-        'plone.app.registry', # For registering the behaviors
-        'reportlab', # PDF label generator
-        'xlutils', # Excel Export
-        'z3c.form >= 2.4.1', # For attributes to work correctly
-
-        'avrc.aeh',
-        'occams.datastore [postgresql]',
-        'beast.browser', # Tools for browser displays
-        ],
-    extras_require=dict(
-        test=['plone.app.testing'],
-        ),
-    entry_points="""
-    [z3c.autoinclude.plugin]
-    target = plone
+    install_requires=REQUIRES,
+    extras_require=EXTRAS,
+    tests_require=EXTRAS['test'],
+    test_suite='nose.collector',
+    entry_points="""\
+    [paste.app_factory]
+    main = occams.lab:main
     """,
-    )
+)
---
+++ src/occams/lab/models.py
@@ -0,0 +1,453 @@
+from sqlalchemy import (
+    Table, Column,
+    ForeignKey, ForeignKeyConstraint, UniqueConstraint, Index,
+    Boolean, Date, Time, Float, Integer, Unicode)
+from sqlalchemy.orm import backref, relationship
+from sqlalchemy.orm.collections import attribute_mapped_collection
+from sqlalchemy.ext.declarative import declared_attr
+
+from occams.clinical.models import Site, Patient, Study, Cycle
+from occams.datastore.models import (
+    ModelClass, Referenceable, Describeable, Modifiable, Auditable)
+
+
+Base = ModelClass('Base')
+
+
+specimentype_study_table = Table(
+    'specimentype_study',
+    Base.metadata,
+    Column('study_id',
+           Integer,
+           ForeignKey(Study.id,
+                      name='fk_specimentype_study_study_id',
+                      ondelete='CASCADE'),
+           primary_key=True),
+    Column('specimentype_id',
+           Integer,
+           ForeignKey('specimentype.id',
+                      name='fk_specimentype_study_specimentype_id',
+                      ondelete='CASCADE'),
+           primary_key=True))
+
+
+specimentype_cycle_table = Table(
+    'specimentype_cycle',
+    Base.metadata,
+    Column('cycle_id',
+           Integer,
+           ForeignKey(Cycle.id,
+                      name='fk_specimentype_cycle_cycle_id',
+                      ondelete='CASCADE'),
+           primary_key=True),
+    Column('specimentype_id',
+           Integer,
+           ForeignKey('specimentype.id',
+                      name='fk_specimentype_cycle_specimentype_id',
+                      ondelete='CASCADE',),
+           primary_key=True))
+
+
+site_lab_location_table = Table(
+    'site_lab_location',
+    Base.metadata,
+    Column('site_id',
+           Integer,
+           ForeignKey(Site.id,
+                      name='fk_site_lab_location_site_id',
+                      ondelete='CASCADE'),
+           primary_key=True),
+    Column('location_id',
+           Integer,
+           ForeignKey('location.id',
+                      name='fk_site_lab_location_location_id',
+                      ondelete='CASCADE')))
+
+
+class SpecimenState(Base, Describeable, Referenceable, Modifiable):
+    """
+    We may wish to add information here about the destination,
+    such as address, contact info, etc.
+    Right now we just need a vocabulary
+    """
+
+    __tablename__ = 'specimenstate'
+
+    @declared_attr
+    def __table_args__(cls):
+        return (
+            UniqueConstraint('name'),)
+
+
+class AliquotState(Base, Describeable, Referenceable, Modifiable):
+    """
+    We may wish to add information here about the destination,
+    such as address, contact info, etc.
+    Right now we just need a vocabulary
+    """
+
+    __tablename__ = 'aliquotstate'
+
+    @declared_attr
+    def __table_args__(cls):
+        return (
+            UniqueConstraint('name'),)
+
+
+class Location(Base, Describeable, Referenceable, Modifiable):
+    """
+    We may wish to add information here about the destination,
+    such as address, contact info, etc.
+    Right now we just need a vocabulary
+    """
+
+    __tablename__ = 'location'
+
+    sites = relationship(
+        Site,
+        secondary=site_lab_location_table,
+        backref=backref(
+            name='lab_location',
+            uselist=False))
+
+    active = Column(
+        Boolean,
+        doc='Flag indicating this location is still active')
+
+    long_title1 = Column(Unicode, doc='Address Line 1')
+
+    long_title2 = Column(Unicode, doc='Address Line 2')
+
+    address_street = Column(Unicode)
+
+    address_city = Column(Unicode)
+
+    address_state = Column(Unicode)
+
+    address_zip = Column(Unicode)
+
+    phone_number = Column(Unicode)
+
+    fax_number = Column(Unicode)
+
+    @declared_attr
+    def __table_args__(cls):
+        return (
+            UniqueConstraint('name'),
+            Index('ix_%s_active' % cls.__tablename__, 'active'))
+
+
+class SpecialInstruction(Base, Describeable, Referenceable, Modifiable):
+    """
+    We may wish to add information here about the special instruction
+    Right now we just need a vocabulary
+    """
+
+    __tablename__ = 'specialinstruction'
+
+    @declared_attr
+    def __table_args__(cls):
+        return (
+            UniqueConstraint('name'), )
+
+
+class SpecimenType(Base, Referenceable, Describeable, Modifiable):
+
+    __tablename__ = 'specimentype'
+
+    tube_type = Column(
+        Unicode,
+        doc='The Type of tube used for this specimen type')
+
+    default_tubes = Column(Integer, doc='Default tubes count')
+
+    studies = relationship(
+        Study,
+        secondary=specimentype_study_table,
+        collection_class=set,
+        backref=backref(
+            name='specimen_types',
+            collection_class=set))
+
+    cycles = relationship(
+        Cycle,
+        secondary=specimentype_cycle_table,
+        collection_class=set,
+        backref=backref(
+            name='specimen_types',
+            collection_class=set))
+
+    # aliquot_types backreffed in AliquotType
+
+
+class AliquotType(Base, Referenceable, Describeable, Modifiable):
+
+    __tablename__ = 'aliquottype'
+
+    specimen_type_id = Column(Integer, nullable=False)
+
+    specimen_type = relationship(
+        SpecimenType,
+        backref=backref(
+            name='aliquot_types',
+            primaryjoin='SpecimenType.id == AliquotType.specimen_type_id',
+            collection_class=attribute_mapped_collection('name'),
+            cascade='all, delete, delete-orphan'),
+        primaryjoin=(specimen_type_id == SpecimenType.id),
+        doc='The Type specimen from which this aliquot type is derived')
+
+    @declared_attr
+    def __table_args__(cls):
+        return (
+            ForeignKeyConstraint(
+                columns=['specimen_type_id'],
+                refcolumns=['specimentype.id'],
+                name='fk_%s_specimentype_id' % cls.__tablename__,
+                ondelete='CASCADE'),
+            UniqueConstraint('specimen_type_id', 'name'),
+            Index('ix_%s_specimen_type_id' % cls.__tablename__,
+                  'specimen_type_id'))
+
+
+class Specimen(Base, Referenceable, Auditable, Modifiable):
+    """
+    Speccialized table for specimen data. Note that only one specimen can be
+    drawn from a patient/protocol/type.
+    """
+
+    __tablename__ = 'specimen'
+
+    specimen_type_id = Column(Integer, nullable=False)
+
+    specimen_type = relationship(
+        SpecimenType,
+        backref=backref(
+            name='specimen',
+            primaryjoin='SpecimenType.id == Specimen.specimen_type_id',
+            cascade='all, delete, delete-orphan'),
+        primaryjoin=(specimen_type_id == SpecimenType.id),
+        doc='The Type specimen')
+
+    patient_id = Column(Integer, nullable=False)
+
+    patient = relationship(
+        Patient,
+        backref=backref(
+            name='specimen',
+            primaryjoin='Patient.id==Specimen.patient_id',
+            cascade='all, delete, delete-orphan'),
+        primaryjoin=(patient_id == Patient.id),
+        doc='The source patient')
+
+    cycle_id = Column(Integer)
+
+    cycle = relationship(
+        Cycle,
+        backref=backref(
+            name='specimen',
+            primaryjoin='Cycle.id==Specimen.cycle_id',
+            cascade='all, delete, delete-orphan'),
+        primaryjoin=(cycle_id == Cycle.id),
+        doc='The cycle for which this specimen was collected')
+
+    state_id = Column(Integer, nullable=False)
+
+    state = relationship(
+        SpecimenState,
+        primaryjoin=(state_id == SpecimenState.id))
+
+    collect_date = Column(Date)
+
+    collect_time = Column(Time)
+
+    location_id = Column(Integer)
+
+    location = relationship(
+        Location,
+        backref=backref(
+            name='specimen',
+            primaryjoin='Location.id==Specimen.location_id'),
+        primaryjoin=(location_id == Location.id),
+        doc='The current location of the specimen')
+
+    previous_location_id = Column(Integer)
+
+    previous_location = relationship(
+        Location,
+        backref=backref(
+            name='previous_specimen',
+            primaryjoin='Location.id==Specimen.previous_location_id'),
+        primaryjoin=(previous_location_id == Location.id),
+        doc='The processing location for the specimen')
+
+    tubes = Column(Integer)
+
+    notes = Column(Unicode)
+
+    @declared_attr
+    def __table_args__(cls):
+        return (
+            ForeignKeyConstraint(
+                columns=['specimen_type_id'],
+                refcolumns=['specimentype.id'],
+                name='fk_%s_specimentype_id' % cls.__tablename__,
+                ondelete='CASCADE'),
+            ForeignKeyConstraint(
+                columns=['patient_id'],
+                refcolumns=[Patient.id],
+                name='fk_%s_patient_id' % cls.__tablename__,
+                ondelete='CASCADE'),
+            ForeignKeyConstraint(
+                columns=['cycle_id'],
+                refcolumns=[Cycle.id],
+                name='fk_%s_cycle_id' % cls.__tablename__,
+                ondelete='SET NULL'),
+            ForeignKeyConstraint(
+                columns=['location_id'],
+                refcolumns=['location.id'],
+                name='fk_%s_location_id' % cls.__tablename__,
+                ondelete='SET NULL'),
+            ForeignKeyConstraint(
+                columns=['previous_location_id'],
+                refcolumns=['location.id'],
+                name='fk_%s_previous_location_id' % cls.__tablename__,
+                ondelete='SET NULL'),
+            ForeignKeyConstraint(
+                columns=['state_id'],
+                refcolumns=['specimenstate.id'],
+                name='fk_%s_specimenstate_id' % cls.__tablename__,
+                ondelete='SET NULL'),
+            Index('ix_%s_specimen_type_id' % cls.__tablename__,
+                  'specimen_type_id'),
+            Index('ix_%s_patient_id' % cls.__tablename__, 'patient_id'),
+            Index('ix_%s_cycle_id' % cls.__tablename__, 'cycle_id'),
+            Index('ix_%s_location_id' % cls.__tablename__, 'location_id'),
+            Index('ix_%s_previous_location_id' % cls.__tablename__,
+                  'previous_location_id'),
+            Index('ix_%s_state_id' % cls.__tablename__, 'state_id'))
+
+
+class Aliquot(Base, Referenceable, Auditable, Modifiable):
+    """
+    Specialized table for aliquot parts generated from a specimen.
+    """
+
+    __tablename__ = 'aliquot'
+
+    specimen_id = Column(Integer, nullable=False)
+
+    specimen = relationship(
+        Specimen,
+        backref=backref(
+            name='aliquot',
+            primaryjoin='Specimen.id == Aliquot.specimen_id',
+            cascade='all, delete, delete-orphan'),
+        primaryjoin=(specimen_id == Specimen.id),
+        doc='The source specimen that this aliquot sample was extracted from')
+
+    aliquot_type_id = Column(Integer, nullable=False)
+
+    aliquot_type = relationship(
+        AliquotType,
+        backref=backref(
+            name='aliquot',
+            primaryjoin='AliquotType.id == Aliquot.aliquot_type_id',
+            cascade='all, delete, delete-orphan'),
+        primaryjoin=(aliquot_type_id == AliquotType.id))
+
+    state_id = Column(Integer, nullable=False)
+
+    state = relationship(
+        AliquotState,
+        primaryjoin=(state_id == AliquotState.id))
+
+    labbook = Column(Unicode, doc='Lab Book number')
+
+    volume = Column(Float, doc='Volume of liquot aliquot')
+
+    cell_amount = Column(Float, doc='Cell count of an aliquot')
+
+    store_date = Column(Date)
+
+    freezer = Column(Unicode)
+
+    rack = Column(Unicode)
+
+    box = Column(Unicode)
+
+    location_id = Column(Integer)
+
+    location = relationship(
+        Location,
+        backref=backref(
+            name='aliquot',
+            primaryjoin='Aliquot.location_id == Location.id'),
+        primaryjoin=(location_id == Location.id))
+
+    previous_location_id = Column(Integer)
+
+    previous_location = relationship(
+        Location,
+        backref=backref(
+            name='stored_aliquot',
+            primaryjoin='Aliquot.previous_location_id == Location.id'),
+        primaryjoin=(previous_location_id == Location.id))
+
+    thawed_num = Column(Integer)
+
+    inventory_date = Column(Date)
+
+    sent_date = Column(Date, doc='Date sent for analysis')
+
+    sent_name = Column(Unicode)
+
+    sent_notes = Column(Unicode)
+
+    notes = Column(Unicode)
+
+    special_instruction_id = Column(Integer)
+
+    special_instruction = relationship(
+        SpecialInstruction,
+        primaryjoin=(special_instruction_id == SpecialInstruction.id))
+
+    @declared_attr
+    def __table_args__(cls):
+        return (
+            ForeignKeyConstraint(
+                columns=['specimen_id'],
+                refcolumns=['specimen.id'],
+                name='fk_%s_specimen_id' % cls.__tablename__,
+                ondelete='CASCADE'),
+            ForeignKeyConstraint(
+                columns=['aliquot_type_id'],
+                refcolumns=['aliquottype.id'],
+                name='fk_%s_aliquottype_id' % cls.__tablename__,
+                ondelete='CASCADE'),
+            ForeignKeyConstraint(
+                columns=['location_id'],
+                refcolumns=['location.id'],
+                name='fk_%s_location_id' % cls.__tablename__,
+                ondelete='SET NULL'),
+            ForeignKeyConstraint(
+                columns=['previous_location_id'],
+                refcolumns=['location.id'],
+                name='fk_%s_previous_location_id' % cls.__tablename__,
+                ondelete='SET NULL'),
+            ForeignKeyConstraint(
+                columns=['special_instruction_id'],
+                refcolumns=['specialinstruction.id'],
+                name='fk_%s_specialinstruction_id' % cls.__tablename__,
+                ondelete='SET NULL'),
+            ForeignKeyConstraint(
+                columns=['state_id'],
+                refcolumns=['aliquotstate.id'],
+                name='fk_%s_aliquotstate_id' % cls.__tablename__,
+                ondelete='SET NULL'),
+            Index('ix_%s_specimen_id' % cls.__tablename__, 'specimen_id'),
+            Index('ix_%s_aliquot_type_id' % cls.__tablename__,
+                  'aliquot_type_id'),
+            Index('ix_%s_location_id' % cls.__tablename__, 'location_id'),
+            Index('ix_%s_previous_location_id' % cls.__tablename__,
+                  'previous_location_id'),
+            Index('ix_%s_state_id' % cls.__tablename__, 'state_id'))
