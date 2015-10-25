8948880809a1a0e2b5326f636441125511b45fa8
Marco Martinez <mam002@ucsd.edu>
2015-10-24 22:03:48 -0700

    Verifies that a user with appriate permissions can print aliquot labels

--- tests/unit/views/test_lab.py
+++ tests/unit/views/test_lab.py
@@ -44,3 +44,41 @@ class Test_build_crud_form:
         form.validate()
 
         assert form.data['specimen'][0]['collect_time'] == time(1, 0)
+
+
+class Test_aliquot_labels:
+
+    def _call_fut(self, *args, **kw):
+        from occams_lims.views.lab import aliquot_labels as view
+        return view(*args, **kw)
+
+    def test_print_when_not_allowed(
+            self, req, db_session, config, factories, check_csrf_token):
+        from pyramid.httpexceptions import HTTPForbidden
+        from webob.multidict import MultiDict
+
+        context = None
+        config.testing_securitypolicy(permissive=False)
+        req.method = 'POST'
+        req.POST = MultiDict()
+
+        with pytest.raises(HTTPForbidden):
+            self._call_fut(context, req)
+
+    def test_print_when_allowed(
+            self, req, db_session, config, factories, check_csrf_token):
+        from webob.multidict import MultiDict
+        from occams_lims.views.lab import ALIQUOT_LABEL_QUEUE
+
+        aliquot = factories.AliquotFactory.create()
+        db_session.flush()
+
+        context = aliquot.location
+        config.testing_securitypolicy(permissive=True)
+        req.session[ALIQUOT_LABEL_QUEUE] = set([aliquot.id])
+        req.method = 'POST'
+        req.POST = MultiDict([('print', '')])
+        res = self._call_fut(context, req)
+
+        assert res.content_type == 'application/pdf'
+        assert len(req.session[ALIQUOT_LABEL_QUEUE]) == 0
