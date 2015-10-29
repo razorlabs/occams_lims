6fbb1b126f6baf3439e16e2981c01d99223791e9
Marco Martinez <mam002@ucsd.edu>
2015-10-28 22:43:24 -0700

    A member of a location can search for samples by PID

--- tests/unit/views/test_aliquot.py
+++ tests/unit/views/test_aliquot.py
@@ -45,3 +45,48 @@ class Test_aliquot_labels:
 
         assert res.content_type == 'application/pdf'
         assert len(req.session[ALIQUOT_LABEL_QUEUE]) == 0
+
+
+class Test_filter_aliquot:
+
+    def _call_fut(self, *args, **kw):
+        from occams_lims.views.aliquot import filter_aliquot as view
+        return view(*args, **kw)
+
+    def test_filter_by_pid_found(self, req, db_session, factories):
+        from webob.multidict import MultiDict
+
+        pid = u'XXX-XXX-XX'
+        state = u'pending'
+        # Samole must be from the current working location
+        location = factories.LocationFactory.create()
+        factories.AliquotFactory.create(
+            state__name=state,
+            location=location,
+            specimen__patient__pid=pid,
+            specimen__location=location)
+        db_session.flush()
+
+        req.GET = MultiDict([('pid', pid)])
+        res = self._call_fut(location, req, state)
+
+        assert res['has_aliquot']
+
+    def test_filter_by_pid_not_found(self, req, db_session, factories):
+        from webob.multidict import MultiDict
+
+        pid = u'XXX-XXX-XX'
+        state = u'pending'
+        # Sample must be from the current working location
+        location = factories.LocationFactory.create()
+        factories.AliquotFactory.create(
+            state__name=state,
+            location=location,
+            specimen__patient__pid=pid,
+            specimen__location=location)
+        db_session.flush()
+
+        req.GET = MultiDict([('pid', u'YYY-YYY-YY')])
+        res = self._call_fut(location, req, state)
+
+        assert not res['has_aliquot']
--- tests/unit/views/test_specimen.py
+++ tests/unit/views/test_specimen.py
@@ -44,3 +44,46 @@ class Test_build_crud_form:
         form.validate()
 
         assert form.data['specimen'][0]['collect_time'] == time(1, 0)
+
+
+class Test_filter_specimen:
+
+    def _call_fut(self, *args, **kw):
+        from occams_lims.views.specimen import filter_specimen as view
+        return view(*args, **kw)
+
+    def test_filter_by_pid_found(self, req, db_session, factories):
+        from webob.multidict import MultiDict
+
+        pid = u'XXX-XXX-XX'
+        state = u'pending'
+        # Sample must be from the current working location
+        location = factories.LocationFactory.create()
+        factories.SpecimenFactory.create(
+            state__name=state,
+            patient__pid=pid,
+            location=location)
+        db_session.flush()
+
+        req.GET = MultiDict([('pid', pid)])
+        res = self._call_fut(location, req, state)
+
+        assert res['has_specimen']
+
+    def test_filter_by_pid_not_found(self, req, db_session, factories):
+        from webob.multidict import MultiDict
+
+        pid = u'XXX-XXX-XX'
+        state = u'pending'
+        # Sample must be from the current working location
+        location = factories.LocationFactory.create()
+        factories.SpecimenFactory.create(
+            state__name=state,
+            patient__pid=pid,
+            location=location)
+        db_session.flush()
+
+        req.GET = MultiDict([('pid', u'YYY-YYY-YY')])
+        res = self._call_fut(location, req, state)
+
+        assert not res['has_specimen']
