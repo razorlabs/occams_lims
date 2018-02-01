8e9c3916863601678c1df248a59031c1cace6e58
Zach Smith <z4smith@ucsd.edu>
2018-02-01 15:49:08 -0800

    Add box route for modal

--- occams_lims/routes.py
+++ occams_lims/routes.py
@@ -29,5 +29,6 @@ def includeme(config):
 
     config.add_route('lims.checkin',            '/{lab}/checkin',          factory=models.LabFactory, traverse='/{lab}')
     config.add_route('lims.checked-in',         '/{lab}/checked-in',       factory=models.LabFactory, traverse='/{lab}')
+    config.add_route('lims.boxes',              '/{lab}/boxes',            factory=models.LabFactory, traverse='/{lab}')
 
     log.debug('Routes configured')
