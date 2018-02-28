5322398d2941c0a9cf889b4a12effecea6531b12
Zach Smith <z4smith@ucsd.edu>
2018-02-28 15:28:19 -0800

    Adds modal template calls and validation for row/column. Box still needed

--- occams_lims/views/checkedin.py
+++ occams_lims/views/checkedin.py
@@ -2,13 +2,14 @@ from pyramid.httpexceptions import HTTPFound, HTTPOk
 from pyramid.view import view_config
 from pyramid.session import check_csrf_token
 import wtforms
+import sqlalchemy as sa
 
 from occams.utils.forms import apply_changes
 
 from .. import _, models
 from ..validators import required_if
 from .aliquot import filter_aliquot
-
+from occams_studies import models as studies
 
 @view_config(
     route_name='lims.checked-in',
@@ -39,10 +40,12 @@ def checked_in(context, request):
             validators=[wtforms.validators.optional()])
         rack = wtforms.StringField(
             validators=[wtforms.validators.optional()])
-        box_row = wtforms.StringField(
-            validators=[wtforms.validators.optional()])
+        box_row = wtforms.IntegerField(
+            validators=[wtforms.validators.optional(),
+                        wtforms.validators.NumberRange(min=1, max=9, message="Please enter number between 1-9")])
         box_column = wtforms.StringField(
-            validators=[wtforms.validators.optional()])
+            validators=[wtforms.validators.optional(),
+                        wtforms.validators.Regexp('[abcdefghi]', message= "Please enter lower case letter between a-i")])
         box = wtforms.StringField(
             validators=[wtforms.validators.optional()])
         thawed_num = wtforms.IntegerField(
@@ -99,40 +102,88 @@ def checked_in(context, request):
 
     return vals
 
-def build_box_form(context, request):
-    db_session = request.db_session
-
-    def row_col_conflict():
-        pass
-
-    class BoxForm(wtforms.Form):
-        row = wtforms.StringField(
-            'row',
-            [wtforms.validators.Optional(),
-             row_col_conflict,
-             wtforms.validators.NumberRange(min=1, max=9)
-            ])
-
-        column = wtforms.StringField(
-            'column',
-            [wtforms.validators.Optional(),
-             row_col_conflict,
-             wtforms.validators.Regexp(
-                '[a-i]', message='row must be between a-i')
-            ])
-
-    return BoxForm
-
 @view_config(
-    route_name='lims.boxes',
+    route_name='lims.boxes_json',
     permission='view',
-    renderer='../templates/checked-in/modal-boxes.pt')
+    renderer='../templates/checked-in/modal-boxes-json.pt')
 def box_grid(context, request):
-    db_session = request.db_session
-    Form = build_box_form(context, request)
-    form = Form(request.POST)
+    """
+        Creates a template return view to host box grid/AJAX html
+    """
+    dummy_view = ''
 
     return {
-        'form': form
+        'data': dummy_view
     }
+
+# AJAX data for box grid modal (modal-boxes-json.pt)
+@view_config(
+    route_name='lims.boxes_ajax',
+    permission='view',
+    renderer='json')
+def box_ajax(context, request):
+    db_session = request.db_session
+    box_query = (
+        db_session.query(models.Aliquot)
+        .filter(models.Aliquot.box != sa.null())
+        .filter(models.Aliquot.box_row != sa.null())
+        .filter(models.Aliquot.box_column != sa.null())
+        )
+
+    def aliquot_abbrev(aliquot_id):
+        """
+            Creates abbrevation of aliquot type for box grid fill
+            See /lims/settings under setup cog
+        """
+        abbrvs = { 1: 'pbmc', 2: 'plsm', 3: 'WB', 4: 'Ur',
+                   5: 'Swb', 6: 'BldSpt', 7: 'BdPlsm',
+                   8: 'swbcvf', 9: 'swbmb', 10: 'swbvm',
+                   11: 'swbCAll', 12: 'swbwck'}
+
+        return abbrvs[aliquot_id]
+
+    def patient_id(specimen_id):
+        """
+            Find the patient OUR by cross referencing parent specimen
+        """
+
+        # get target specimen
+        specimen_query = (
+            db_session.query(models.Specimen)
+            .filter_by(id=specimen_id)
+            .one())
+
+        # get patient id
+        patient_id = specimen_query.patient_id
+
+        # get patient our
+        patient_our = (
+            db_session.query(studies.Patient)
+            .filter_by(id=patient_id)
+            .one())
+
+        our = patient_our.pid
+
+        return our
+
+    assign_aliquot = box_query.all()
+    aliquot_dict = {}
+
+    # Build dict for ajax return
+    for aliquot in assign_aliquot:
+        our = patient_id(aliquot.specimen_id)
+        aliquot_type = aliquot.aliquot_type_id
+        aliquot_abbr = aliquot_abbrev(aliquot_type)
+
+        abbr = ''.join([our, ' ', aliquot_abbr])
+
+        aliquot_dict[aliquot.id] = {
+                'box'     : aliquot.box,
+                'box_row' : aliquot.box_row,
+                'box_col' : aliquot.box_column,
+                'abbr'    : abbr
+                }
+
+    # Accessed in modal-boxes-json.pt for js box-grid functions
+    return { 'aliquot' : aliquot_dict }
 
